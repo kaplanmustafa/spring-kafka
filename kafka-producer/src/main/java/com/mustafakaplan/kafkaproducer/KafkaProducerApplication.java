0f69ae35850e5857d47951a08c64f11632b6b1be
muskaplan <muskaplan@innova.com.tr>
2022-09-28 13:56:51 +0300

    producer application

--- kafka-producer/src/main/java/com/mustafakaplan/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/mustafakaplan/kafkaproducer/KafkaProducerApplication.java
@@ -1,13 +1,32 @@
 package com.mustafakaplan.kafkaproducer;
 
+import com.mustafakaplan.kafkaproducer.service.ProducerService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaProducerApplication {
+public class KafkaProducerApplication implements ApplicationRunner {
+
+    private final ProducerService producerService;
+
+    public KafkaProducerApplication(ProducerService producerService) {
+        this.producerService = producerService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaProducerApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println("Kafka log send starting...");
+
+        for (int i = 0; i < 10; i++) {
+            producerService.sendMessage(String.format("[%d] -- Hello World", i));
+        }
+
+        System.out.println("Kafka log sent...");
+    }
 }
